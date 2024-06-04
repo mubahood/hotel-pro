5a09e91b85da3b46cb96f4200da25f3ccb0fbd05
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2024-06-04 20:39:23 +0300

    added assets

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOTEL PRO</title>
+</head>
+
+<body>
+    <h1>Welcome to Hotel Pro</h1>
+    <p>Hotel Pro is a hotel booking website that allows you to book hotels in different locations around the world.</p>
+    <p>Click <a href="login.php">here</a> to login to your account.</p>
+    <p>Click <a href="register.php">here</a> to register for an account.</p>
+</body>
+
+</html>
